c54271a7bb6842d15334440596094dd7e43b86d8
Spandan Kumar Sahu <spandankumarsahu@gmail.com>
2018-01-16 01:36:07 +0530

    add all Tender APIs
    Signed-off-by: Spandan Kumar Sahu <spandankumarsahu@gmail.com>

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ var url = require('url');
 var stock = '150'
 var sensor = '5.0'
 
-var Issued_Tenders = []
-var Vendor_Tenders = []
-var ApprovedVendor_Tenders = []
-
 var updateStock = function(req, res){
     stock = req.query.stock;
     res.json({stock: stock});
@@ -19,6 +15,51 @@ var updateSensor = function(req, res){
     sensor = req.query.sensor;
     res.json({sensor: sensor});
 };
+
+var Issued_Tenders = {}
+var Applied_Tenders = {}
+var Approved_Tenders = {}
+
+var issueTender = function(req, res){
+    tenderID = req.query.tenderID;
+    tender = req.query.tender;
+    Issued_Tenders[tenderID] = tender;
+    res.json({success: true});
+}
+
+var approveTender = function(req, res){
+    tenderID = req.query.tenderID;
+    if (Applied_Tenders[tenderID] == null){
+	res.json({success: false, message: "Incorrect Tender ID"});
+    } else{
+	Approved_Tenders[tenderID] = Applied_Tenders[tenderID];
+	res.json({success: true});
+    }
+}
+
+var applyTender = function(req, res){
+    tenderID = req.query.tenderID;
+    if (Issued_Tenders[tenderID] == null){
+	res.json({success: false, message: "Incorrect Tender ID"});
+    } else{
+	Applied_Tenders[tenderID] = Issued_Tenders[tenderID];
+	res.json({success: true});
+    }
+}
+
+var viewAllTenders = function(req, res){
+    res.json({Issued: Issued_Tenders, Applied: Applied_Tenders, Approved: Approved_Tenders});
+}
+var viewIssuedTenders = function(req, res){
+    res.json({Issued: Issued_Tenders});
+}
+var viewApprovedTenders = function(req, res){
+    res.json({Approved: Approved_Tenders});
+}
+var viewAppliedTenders = function(req, res){
+    res.json({Applied: Applied_Tenders});
+}
+
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
@@ -31,8 +72,15 @@ express()
     .get('/sensor', (req, res) => res.json({sensor: sensor}))
     .get('/stock_update', updateStock)
     .get('/sensor_update', updateSensor)
-    //.post('/admin/issueTender')
-    //.post('/admin/approveTender')
-    //.post('/vendor/applyTender')
-    //.get('/vendor/getNewTender')
+    .get('/admin/issueTender', issueTender)
+    .get('/admin/approveTender', approveTender)
+    .get('/vendor/applyTender', applyTender)
+    .get('/vendor/viewAllTenders', viewAllTenders)
+    .get('/admin/viewAllTenders', viewAllTenders)
+    .get('/vendor/viewIssuedTenders', viewIssuedTenders)
+    .get('/admin/viewIssuedTenders', viewIssuedTenders)
+    .get('/vendor/viewApprovedTenders', viewApprovedTenders)
+    .get('/admin/viewApprovedTenders', viewApprovedTenders)
+    .get('/vendor/viewAppliedTenders', viewAppliedTenders)
+    .get('/admin/viewAppliedTenders', viewAppliedTenders)
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))

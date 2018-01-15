098b8a53b17fbcfc17d730bc209490146cc41af8
Spandan Kumar Sahu <spandankumarsahu@gmail.com>
2018-01-16 00:12:20 +0530

    add update methods for sensor and stock data
    Signed-off-by: Spandan Kumar Sahu <spandankumarsahu@gmail.com>

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var url = require('url');
 
 var stock = '150'
+var sensor = '5.0'
 
-    
+var Issued_Tenders = []
+var Vendor_Tenders = []
+var ApprovedVendor_Tenders = []
+
+var updateStock = function(req, res){
+    stock = req.query.stock;
+    res.json({stock: stock});
+};
+
+var updateSensor = function(req, res){
+    sensor = req.query.sensor;
+    res.json({sensor: sensor});
+};
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
@@ -14,10 +28,11 @@ express()
     .get('/vendor', (req, res) => res.sendFile(path.join(__dirname, 'views/pages/index.html')))
     .get('/admin', (req, res) => res.sendFile(path.join(__dirname, 'views/pages/index.html')))
     .get('/stock', (req, res) => res.json({stock: stock}))
-    //.get('/sensor',)
+    .get('/sensor', (req, res) => res.json({sensor: sensor}))
+    .get('/stock_update', updateStock)
+    .get('/sensor_update', updateSensor)
     //.post('/admin/issueTender')
     //.post('/admin/approveTender')
     //.post('/vendor/applyTender')
     //.get('/vendor/getNewTender')
-    //.post('/sensor_update', (req, res) => res.send(sensor))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
